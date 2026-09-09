466667bfe8939036836224e4174e98478865d96b
logoriphm <141328840+logoriphm@users.noreply.github.com>
2026-09-09 22:05:21 +0500

    Add files via upload

---
+++ lab1.swift
@@ -0,0 +1,20 @@
+let firstName: String = "Daulet"
+let lastName: String = "Bazarbay"
+let birthYear: Int = 2006
+let currentYear: Int = 2026
+let age: Int = currentYear - birthYear
+let isStudent: Bool = true
+let height: Double = 1.76
+let hometown: String = "Almaty"
+
+var hobby: String = "run"
+var numberOfHobbies: Int = 3
+var favoriteNumber: Int = 7
+var isHobbyCreative: Bool = true
+var favoriteSubject: String = "math"
+
+let futureGoals: String = "In the future, I want to have a large farm."
+
+let lifeStory = "My name is \(firstName) \(lastName). I am \(age) years old, born in \(birthYear), and living in \(hometown). I am currently a student: \(isStudent). My height is \(height)m. I enjoy \(hobby), which is a creative hobby: \(isHobbyCreative). I have \(numberOfHobbies) hobbies in total, my favorite number is \(favoriteNumber), and my favorite subject is \(favoriteSubject). \(futureGoals)"
+
+print(lifeStory)
